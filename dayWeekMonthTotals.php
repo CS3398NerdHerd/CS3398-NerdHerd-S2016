bf9ebdf84cd24601aa63b355737e10902503c44b
schadhoffman <1schadhoffman@gmail.com>
2016-04-14 14:17:37 -0500

    Added files via upload

---
+++ dayWeekMonthTotals.php
@@ -0,0 +1,18 @@
+
+function addDayTotal($a, $b) {// this function returns the total for day.
+     
+  $c = $a + $b;// $a is the day tottal $b is the amount to add from the consumed food.
+    return $c; returns //the total for day.
+}
+
+ addWeekTotal($c,$d ){// this function returns total for week
+     $e = $c + $d;// $c is the day total $d total for the week.
+    return $e; // $e is the total add.
+}
+  
+addMonth($e,$f ){ // this function returns total for month
+     $g = $e + $f;// $e is the week total $f total for the week.
+    return $g; // $g is the total add for month.
+}
+
+ 
