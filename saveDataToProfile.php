1f7db60e42256b605a268aa0b3445a10a2a75b91
schadhoffman <1schadhoffman@gmail.com>
2016-04-14 15:21:53 -0500

    Added files via upload

---
+++ saveDataToProfile.php
@@ -0,0 +1,11 @@
+add_action( 'personal_options_update', 'my_save_extra_profile_fields' );
+add_action( 'edit_user_profile_update', 'my_save_extra_profile_fields' );
+
+function my_save_extra_profile_fields( $user_id ) {
+
+	if ( !current_user_can( 'edit_user', $user_id ) )
+		return false;
+
+	/* Copy and paste this line for additional fields. Make sure to change 'dayTotals' to the field ID. */
+	update_usermeta( $user_id, 'dayTotals', $_POST['dayTotals'] );
+}
